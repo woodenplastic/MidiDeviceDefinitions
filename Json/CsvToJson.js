787478147a42725a57403645b81575c8fe696d74
Adi <138585731+woodenplastic@users.noreply.github.com>
2025-09-24 01:54:17 +0200

    device icon fallback is none

--- Json/CsvToJson.js
+++ Json/CsvToJson.js
@@ -241,7 +241,7 @@ function convertDatabase() {
 
                     if (!database[manufacturer][deviceName]) {
                         database[manufacturer][deviceName] = {
-                            device_icon: "",
+                            device_icon: param.device_icon || "none",
                             midi_thru: "",
                             midi_in: "",
                             midi_clock: "",
